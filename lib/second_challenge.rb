4f9c5d2e8306a7c21f19dc37a557d5c70bae3ad1
Alethia Quintero <alethiaq22@gmail.com>
2019-07-19 17:42:17 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,13 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
+      patriarch: {},
+      matriarch: {},
+      hero: {},
       }, 
     :capulet => {
       #your key/value pairs here!
       }}
 
-  
+
 end
